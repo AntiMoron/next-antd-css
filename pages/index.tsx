5b7163d811344c0ed2d241ea5852438573c701a5
Boyuan Gao <anti2moron@gmail.com>
2023-03-17 18:24:30 +0800

    init

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import {Input } from 'antd'
 import styles from '@/styles/Home.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -16,6 +17,7 @@ export default function Home() {
       </Head>
       <main className={styles.main}>
         <div className={styles.description}>
+          <Input />
           <p>
             Get started by editing&nbsp;
             <code className={styles.code}>pages/index.tsx</code>
